c3c1a9d3cd57bc513308c7232ddc3ce76825da54
eyza576 <eyza_10f@hotmail.com>
2022-04-28 22:51:08 -0500

    examen

--- src/main/java/ilp/edu/RuthE/controller/PruebaController.java
+++ src/main/java/ilp/edu/RuthE/controller/PruebaController.java
@@ -5,11 +5,13 @@ import ilp.edu.RuthE.entity.Persona;
 import ilp.edu.RuthE.service.IPersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("login")
 public class PruebaController {
     @Autowired
     private IPersonaService personaService;
